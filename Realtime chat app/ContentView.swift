de5754a46e151e0d1bd85ebdeb5152a38bcdf1e8
himash1997 <31865243+himash1997@users.noreply.github.com>
2020-03-11 16:01:15 +0530

    Initial Commit

---
+++ Realtime chat app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Realtime chat app
+//
+//  Created by Himash Nadeeshan on 3/11/20.
+//  Copyright © 2020 Himash Nadeeshan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
