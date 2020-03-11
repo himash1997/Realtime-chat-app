177dff956f52275d54bb6850b73a9a2bc2edc346
himash1997 <31865243+himash1997@users.noreply.github.com>
2020-03-11 17:22:29 +0530

    ui setup

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'Realtime chat app' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Realtime chat app
+  pod 'Firebase/Auth'
+
+end
--- Realtime chat app/ContentView.swift
+++ Realtime chat app/ContentView.swift
@@ -16,6 +16,101 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Verify()
+    }
+}
+
+struct Getnumber : View {
+    
+    @State var number = ""
+    @State var code = ""
+    @State var show = false
+    @State var msg = ""
+    @State var alert = false
+    @State var ID = ""
+    
+    var body: some View{
+        
+        VStack(spacing: 20){
+            
+            Image("images")
+            
+            Text("Welcome!")
+            .font(.largeTitle)
+            .fontWeight(.heavy)
+            
+            Text("Please enter your number to verify your account")
+                .font(.body)
+                .foregroundColor(Color.gray)
+                .padding(.bottom,40)
+            
+            HStack{
+                TextField("+94",text: $code)
+                .keyboardType(.numberPad)
+                .frame(width : 40)
+                .padding()
+                .background(Color("Color"))
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                
+                TextField("Number",text: $number)
+                .keyboardType(.numberPad)
+                .padding()
+                .background(Color("Color"))
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                
+            }.padding()
+            
+            Button(action: {
+                
+            }){
+                Text("Send")
+                .frame(width: 380, height: 50)
+            }.foregroundColor(.white)
+            .background(Color.blue)
+            .cornerRadius(10)
+        }
+    }
+}
+
+
+struct Verify : View {
+    
+//    @Binding var show : Bool
+//    @Binding var ID : String
+    
+    @State var msg = ""
+    @State var alert = false
+    @State var verificationcode = ""
+    
+    var body : some View{
+        VStack(spacing: 20){
+            
+            Image("images")
+            
+            Text("Welcome!")
+            .font(.largeTitle)
+            .fontWeight(.heavy)
+            
+            Text("Please enter your verification number")
+                .font(.body)
+                .foregroundColor(Color.gray)
+                .padding(.bottom,40)
+                
+            TextField("Code",text: $verificationcode)
+            .keyboardType(.numberPad)
+            .padding()
+            .background(Color("Color"))
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .padding()
+                
+            Button(action: {
+                
+            }){
+                Text("Verify")
+                .frame(width: 380, height: 50)
+            }.foregroundColor(.white)
+            .background(Color.blue)
+            .cornerRadius(10)
+        }
     }
 }
